ded53bd8fbebf072432c1ba9b8ddd88425dec960
Alfonso Tienda <afoone@hotmail.com>
2021-03-09 13:51:55 +0100

    redirect

--- 08-facturas/src/components/InvoiceForm.jsx
+++ 08-facturas/src/components/InvoiceForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Redirect} from 'react-router-dom'
 
 
@@ -6,15 +6,20 @@ const InvoiceForm = () => {
 
     // crear una propiedad del estado para cada input
     // crear una propiedad del estado para si tengo o no que redirigir.
+    const [redirect, setRedirect] = useState(false)
 
     
     // guardar()
     // 1. hacer el post
     // Si todo ha ido bien -> poner el estado de redirect a true
 
+    const cancelar = () => {
+        setRedirect(true)
+    }
+
     return (
         <div>
-            {false && <Redirect to="/"/>}
+            {redirect && <Redirect to="/"/>}
             
             Crearemos un formulario con 3 inputs:
             - cliente (texto)
@@ -24,6 +29,8 @@ const InvoiceForm = () => {
             Botón guardar: que hará en axios.post correspondiente (haría llamada a guardar())
             Botón cancelar: que hará el redirect
             formulario
+<i className="reply icon" onClick={cancelar}></i>
+            <button className="ui negative button" onClick={cancelar}>Cancelar</button>
         </div>
     )
 }
